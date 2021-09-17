dd209898bb50c9683c8fbd6b63158915a6dea2be
03Sandraa <90039351+03Sandraa@users.noreply.github.com>
2021-09-17 21:15:48 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+const buttonHamburgermenuGebruikers = document.querySelector(".hamburger");
+const Gebruikersmenu = document.querySelector(".gebruikersmenu ul"); 
+
+buttonHamburgermenuGebruikers.addEventListener("click",openGebruikersmenu);
+
+function openGebruikersmenu(){
+    Gebruikersmenu.classList.toggle("hidden");
+};
