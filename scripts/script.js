147a0a95963e2676e704b4553d38a0302e3c0ec5
03Sandraa <90039351+03Sandraa@users.noreply.github.com>
2021-09-22 17:49:24 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,22 @@
 // JavaScript Document
 const buttonHamburgermenuGebruikers = document.querySelector(".hamburger");
 const Gebruikersmenu = document.querySelector(".gebruikersmenu ul"); 
+const ButtonNavComputer = document.querySelector(".ButtonNavComputer");
+const NavComputer = document.querySelector(".navComputer");
+const ButtonPlaystation = document.querySelector(".ButtonPlaystation");
+const NavPlaystation = document.querySelector(".navPlaystation");
+const ButtonNavXbox = document.querySelector(".ButtonNavXbox");
+const NavXbox = document.querySelector(".navXbox");
+const ButtonNavNintendo = document.querySelector(".ButtonNavNintendo");
+const NavNintendo = document.querySelector(".navNintendo");
 
 buttonHamburgermenuGebruikers.addEventListener("click",openGebruikersmenu);
+ButtonNavComputer.addEventListener("click",openPCmenu);
 
 function openGebruikersmenu(){
     Gebruikersmenu.classList.toggle("hidden");
 };
+
+function openPCmenu(){
+    NavComputer.classList.toggle("hidden");
+}
